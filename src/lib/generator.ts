778ea11500f871b8016f065a4ff88c2934aeef51
Kaden Thomas <thomas.kaden4@outlook.com>
2024-01-09 02:09:10 -0800

    Fix easings in and out frames

--- src/lib/generator.ts
+++ src/lib/generator.ts
@@ -83,7 +83,7 @@ export function generateKeyFrameJSON(settings: Settings) {
   return [
     {
       DisplayName: "Generated easing for " + settings.ease.name,
-      in: 0,
+      in: settings.start.frame,
       max: 0,
       min: 0,
       name: "rect",
@@ -94,7 +94,7 @@ export function generateKeyFrameJSON(settings: Settings) {
     },
     {
       DisplayName: "Generated rotation easing for " + settings.ease.name,
-      in: 0,
+      in: settings.start.frame,
       max: 360,
       min: -360,
       name: "rotation",
